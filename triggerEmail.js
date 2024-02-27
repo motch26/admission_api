adaa333ed6d5d5e52b2ffb7d375b28599c45edfe
almark <motch26@gmail.com>
2024-02-27 16:38:05 +0800

    new filter of no entries

--- triggerEmail.js
+++ triggerEmail.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const entries = require("./email.json");
 const axios = require("axios");
 const pool = require("./config");
 
@@ -7,8 +8,49 @@ const getEmailsWithNoEntries = async () => {
 
   try {
     const conn = await pool.getConnection();
-    let sql =
-      "SELECT * FROM emails WHERE email NOT IN (SELECT email FROM entries)";
+    let sql = `SELECT * FROM emails WHERE 
+(email LIKE '%gmail.com' OR
+email LIKE '%cpac.edu.ph' OR
+email LIKE '%dbti-victorias.edu.ph' OR
+email LIKE '%icloud.com' OR
+email LIKE '%usls.edu.ph' OR
+email LIKE '%yahoo.com' OR
+email LIKE '%depedqc.ph' OR
+email LIKE '%outlook.com' OR	
+email LIKE '%deped.gov.ph' OR
+email LIKE '%btths.edu.ph' OR
+email LIKE '%cpu.edu.ph' OR
+email LIKE '%csav.edu.ph' OR
+email LIKE '%scssbacolod.edu.ph' OR
+email LIKE '%hotmail.com' OR
+email LIKE '%csab.edu.ph' OR
+email LIKE '%depedmakati.ph' OR
+email LIKE '%adventistacademy-bacolod.com' OR
+email LIKE '%gov.com.ph' OR
+email LIKE '%rocketmail.com' OR
+email LIKE '%riversidestudent.edu.ph' OR
+email LIKE '%megaworldcorp.com' OR
+email LIKE '%pcaat.edu.ph' OR
+email LIKE '%yeyinitiative.org' OR
+email LIKE '%wnu.sti.edu.ph' OR
+email LIKE '%mendez-dental.com' OR
+email LIKE '%yandex.com' OR
+email LIKE '%lccbonline.edu.ph' OR
+ email LIKE '%ubiquity.com' OR
+ email LIKE '%ncr2.deped.gov.ph' OR
+ email LIKE '%lccb.edu.ph' OR
+ email LIKE '%lccb.online.edu.ph' OR
+ email LIKE '%mabinicolleges.edu.ph' OR
+ email LIKE '%depedparanaquecity.com' OR
+ email LIKE '%cvsu.edu.ph' OR
+ email LIKE '%nwtf.org.ph' OR
+ email LIKE '%outlook.ph' OR
+ email LIKE '%student.uno-r.edu.ph' OR
+ email LIKE '%fbc.com.ph' OR
+ email LIKE '%csab.edu.com' 
+) 
+AND
+email NOT IN (SELECT email FROM entries);`;
     const [rows] = await conn.execute(sql);
 
     return rows;
@@ -19,19 +61,29 @@ const getEmailsWithNoEntries = async () => {
   }
 };
 const triggerEmail = async (email, uuid) => {
-  await axios.post(`https://admission.chmsu.edu.ph/api/sendEmail.php`, {
-    email,
-    uuid,
-    frontURL: "https://admission2024.chmsu.edu.ph",
-  });
+  await axios.post(
+    `https://admission.chmsu.edu.ph/api/sendEmailToChangeSchedule.php`,
+    {
+      email,
+      uuid,
+    }
+  );
 };
-(async () => {
-  const emails = await getEmailsWithNoEntries();
-  emails.push({ email: "motch26@gmail.com", uuid: "new" });
-  const extractedEmails = emails.slice(0, 5000);
-  for (const email of extractedEmails) {
-    await triggerEmail(email.email, email.uuid);
-    console.log("email sent to", email);
+const sendEmailWithUpdates = async () => {
+  for (const entry of entries) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await triggerEmail(entry.email, entry.uuid);
+    console.log("email sent to", entry);
   }
-  console.log("Done");
-})();
+};
+sendEmailWithUpdates();
+// (async () => {
+//   const emails = await getEmailsWithNoEntries();
+//   emails.push({ email: "motch26@gmail.com", uuid: "new" });
+//   const extractedEmails = emails.slice(0, 5000);
+//   for (const email of extractedEmails) {
+//     await triggerEmail(email.email, email.uuid);
+//     console.log("email sent to", email);
+//   }
+//   console.log("Done");
+// })();
